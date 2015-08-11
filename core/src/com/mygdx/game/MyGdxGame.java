109adde41b3e79ef798acc5dfc0fe92a9ae28254
kjm <minn6534@gmail.com>
2015-08-11 17:43:15 +0900

    fix texture rendering issue, and change block.tmx : 64 x 64 -> 32 x 32

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -86,20 +86,23 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
 
     @Override
     public void render () {
-        spriteBatch.begin();
 
         Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+
+        camera.update();
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+        
+
+        spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.begin();
         for(int i = 0; i < unitArrayList.size(); i++){
             Unit newunit = unitArrayList.get(i);
             newunit.render();
         }
-
-        tiledMapRenderer.setView(camera);
-        tiledMapRenderer.render();
-        camera.update();
-
         spriteBatch.end();
     }
 
